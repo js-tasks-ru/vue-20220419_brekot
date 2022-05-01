8e0f7d57f11ff616dd74affbef8f5493c9edde60
Andrey Brekotkin <brekot@ya.ru>
2022-05-01 19:34:23 +0300

    Правка блока 4 SFC

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -19,7 +19,7 @@ export default {
 
   computed: {
     bg: function () {
-      return 'url(' + this.image + ')';
+      return this.image ? 'url(' + this.image + ')' : 'var(--default-cover)';
     },
   },
 };
@@ -29,7 +29,7 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(bg), var(--default-cover);
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(bg);
   display: flex;
   flex-direction: column;
   align-items: center;
